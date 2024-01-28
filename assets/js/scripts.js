ac7fa8b81f03e1da86e955dd8dc3834603ddd2a3
Christian Chacón <cchaconb@gmail.com>
2024-01-28 17:42:46 -0300

    cierre-carrusel

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+const myCarouselElement = document.querySelector('#carrusel_Ricomida')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+// jq procesos aplicados
+
+$(function () {
+
+
+});
