3c9b1615bd9b2d5ec417329463a0465809a365d1
Christian Chacón <cchaconb@gmail.com>
2024-01-28 23:06:20 -0300

    cierre de hero section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,18 @@
-const myCarouselElement = document.querySelector('#carrusel_Ricomida')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
-
-// jq procesos aplicados
-
 $(function () {
+  
+  const myCarouselElement = document.querySelector('#carrusel_Ricomida')
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    touch: false
+  })
+  
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  
+  // jq procesos aplicados
+$("#enviarCorreo").click(function () {
+    alert("El correo fue enviado correctamente...");
+});
 
 
 });
