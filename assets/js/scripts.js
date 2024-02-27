92b34788f62008c7f1e6ccbc02996272c93a97e6
Christian Chacón <chacb.dev@gmail.com>
2024-02-27 17:50:38 -0300

    version1 peñuelasPropiedades

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,23 +10,5 @@ $(function () {
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
   // jq procesos aplicados
-  $("#enviarCorreo").click(function () {
-    alert("El correo fue enviado correctamente...");
-  });
-  // se coloca el titulo en rojo al hacer doble click
-  $(".ingredientes>H3").dblclick(function () {
-    $(".preparacion>H3").css("background-color", "white")
-    $(".ingredientes>H3").css("background-color", "red")
-  });
 
-  $(".preparacion>H3").dblclick(function () {
-
-    $(".ingredientes>H3").css("background-color", "white")
-    $(".preparacion>H3").css("background-color", "red")
-  });
-
-  // se ocultan los p de las tarjetas
-  $(".tarjetas").click(function () {
-    $(".tarjetas+p").toggle(2000)
-  });
 });
